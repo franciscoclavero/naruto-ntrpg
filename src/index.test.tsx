b6268519e4d61eac5081d958e9ba790512ceea18
Francisco Kluser <franciscogkoclavero@gmail.com>
2023-02-09 10:54:09 -0300

    feat: install jest

---
+++ src/index.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+
+import App from './App'
+
+describe('teste ', () => {
+  it('teste', () => {
+    render(<App />)
+
+    const h1Node = screen.getByText('New Project')
+    expect(h1Node).toBeInTheDocument()
+  })
+})
