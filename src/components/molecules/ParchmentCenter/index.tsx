78aee3bf963eefb612eab2220b362203bdddc740
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-17 15:05:11 -0300

    feat: create parchment center style

--- src/components/molecules/ParchmentCenter/index.tsx
+++ src/components/molecules/ParchmentCenter/index.tsx
@@ -1,7 +1,32 @@
 import React from 'react'
 
-const ParchmentCenter = () => {
-  return <div></div>
+import { ParchmentLabelCenter } from './style.module'
+
+export type TParchmentLabel = {
+  width: string
+  backgroundColor?: string
+  borderColor?: string
+  borderSize?: string
+  textContent: string
+}
+
+const ParchmentCenter = ({
+  textContent,
+  width,
+  backgroundColor = 'white',
+  borderColor = 'transparent',
+  borderSize = '0px',
+}: TParchmentLabel) => {
+  return (
+    <ParchmentLabelCenter
+      width={width}
+      backgroundColor={backgroundColor}
+      borderColor={borderColor}
+      borderSize={borderSize}
+    >
+      {textContent}
+    </ParchmentLabelCenter>
+  )
 }
 
 export default ParchmentCenter
