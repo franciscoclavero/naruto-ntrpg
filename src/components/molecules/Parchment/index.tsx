192f592eb30bfdbbc325b81095e5c7d06fcf861e
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-17 15:16:37 -0300

    feat: create style parchment

--- src/components/molecules/Parchment/index.tsx
+++ src/components/molecules/Parchment/index.tsx
@@ -1,7 +1,37 @@
 import React from 'react'
+import ParchmentCenter, { TParchmentLabel } from '../ParchmentCenter'
+import ParchmentRoll from '../ParchmentRoll'
 
-const Parchment = () => {
-  return <div> Pergaminho </div>
+import { ParchmentBody } from './style.module'
+
+interface IParchment {
+  height: string
+  width: string
+  backgroundColor: string
+  parchmentCenter: TParchmentLabel
+}
+
+const Parchment = ({ height, width, backgroundColor, parchmentCenter }: IParchment) => {
+  return (
+    <ParchmentBody height={height} width={width} backgroundColor={backgroundColor}>
+      <ParchmentRoll height={height} left='-14px' />
+      <ParchmentCenter
+        textContent={parchmentCenter.textContent}
+        width={parchmentCenter.width}
+        backgroundColor={parchmentCenter.backgroundColor}
+        borderSize={parchmentCenter.borderSize}
+        borderColor={parchmentCenter.borderColor}
+      />
+      <ParchmentRoll height={height} left='14px' />
+    </ParchmentBody>
+  )
 }
 
 export default Parchment
+
+//
+// width={parchmentLabel.width}
+// backgroundColor={parchmentLabel.backgroundColor}
+// borderColor={parchmentLabel.borderColor}
+// borderSize={parchmentLabel.borderSize}
+// textContent={parchmentLabel.textContent}
