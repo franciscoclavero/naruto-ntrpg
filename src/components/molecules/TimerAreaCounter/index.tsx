7b7afcaef3dbcd284fd69e336f4dc11590818796
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-21 16:48:49 -0300

    feat: create timer

--- src/components/molecules/TimerAreaCounter/index.tsx
+++ src/components/molecules/TimerAreaCounter/index.tsx
@@ -1,24 +1,23 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
+import React, { useEffect, useState } from 'react'
 import { useAppSelector } from '../../../redux/hooks/useSelector'
 
-import { setInitialPosition } from '../../../redux/reducers/timerReducer'
 import { TimeAreaCounterStyled, TimerText } from './style.module'
 
 const TimerAreaCounter = () => {
   const timer = useAppSelector((state) => state.timer)
-  const dispatch = useDispatch()
+  const [counterPosition, setCounterPosition] = useState((100 * timer.initialPosition) / timer.time)
 
   useEffect(() => {
-    setTimeout(() => {
-      dispatch(setInitialPosition(0))
-      clearTimeout
-    }, 1000)
-  }, [timer.initialPosition])
+    setCounterPosition(0)
+  }, [])
 
   return (
     <>
-      <TimeAreaCounterStyled initialPosition={timer.initialPosition} time={timer.time} />
+      <TimeAreaCounterStyled
+        counterPosition={counterPosition}
+        time={timer.time}
+        initialPosition={timer.initialPosition}
+      />
       <TimerText width={timer.width}> {timer.labelText} </TimerText>
     </>
   )
--- src/components/molecules/TimerAreaCounter/style.module.tsx
+++ src/components/molecules/TimerAreaCounter/style.module.tsx
@@ -1,18 +1,26 @@
 import styled from 'styled-components'
 
 interface ITimerAreaCounter {
-  initialPosition: number
+  counterPosition: number
   time: number
+  initialPosition: number
 }
 
 export const TimeAreaCounterStyled = styled.div<ITimerAreaCounter>`
   background-color: #f05321;
   height: 100%;
-  width: calc((${(props) => props.initialPosition} / ${(props) => props.time}) * 100%);
+  width: ${(props) => props.counterPosition}%;
 
-  transition: width ${(props) => props.time}s linear;
+  ${(props) => {
+    let transition = ''
+
+    if (props.counterPosition === 0)
+      transition = `transition: width ${props.initialPosition}s linear`
+    if (props.counterPosition > 0) transition = `transition: width 0s linear`
+
+    return transition
+  }}
 `
-
 interface ITimerAreaText {
   width: string
 }
@@ -20,10 +28,10 @@ interface ITimerAreaText {
 export const TimerText = styled.span<ITimerAreaText>`
   color: #fff;
   display: block;
+  position: absolute;
   font-weight: bold;
   height: 44px;
   line-height: 44px;
-  position: absolute;
   text-align: center;
   width: ${(props) => props.width};
 `
