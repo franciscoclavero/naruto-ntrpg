706c4dfb7d36c2b45092a4a0a333c744f685f2a0
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-21 09:14:33 -0300

    refactor: recive prop

--- src/components/molecules/TimerArea/style.module.tsx
+++ src/components/molecules/TimerArea/style.module.tsx
@@ -1,8 +1,14 @@
 import styled from 'styled-components'
 
-export const TimeAreaStyled = styled.div`
+interface ITimerArea {
+  width: string
+}
+
+export const TimeAreaStyled = styled.div<ITimerArea>`
   background-color: #fcba21;
   border: 3px solid #024da1;
   border-left: none;
+  display: flex;
   height: 50px;
+  width: ${(prop) => prop.width};
 `
