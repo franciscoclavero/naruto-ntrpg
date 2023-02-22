6f2965d162f482d4259f8b572ddaf53a1373e7b8
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-22 09:22:49 -0300

    feat: create TimerArea test

---
+++ src/components/molecules/TimerArea/index.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import React from 'react'
+
+import TimerArea from './'
+import { store } from '../../../redux/store'
+import { Provider } from 'react-redux'
+
+describe('TimerArea component', () => {
+  it('- render', () => {
+    const { container } = render(
+      <Provider store={store}>
+        <TimerArea />
+      </Provider>,
+    )
+
+    const timerAreaDivNode = container.getElementsByTagName('div')
+    const timerAreaSpanNode = container.getElementsByTagName('span')
+
+    expect(timerAreaDivNode.length).toBe(2)
+    expect(timerAreaSpanNode.length).toBe(1)
+  })
+})
