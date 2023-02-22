7a1a2bc72f31f971d8c2a40485e67e24a2790f8c
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-22 14:44:31 -0300

    feat: change to use props

--- src/components/molecules/CharacterDataArea/index.tsx
+++ src/components/molecules/CharacterDataArea/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
+import { charactersList } from '../../../DB/charactersList'
+import { getItem } from '../../../functions/getItem'
+import { ICharacter } from '../CharacterArea'
 import CharacterEffectsArea from '../CharacterEffectsArea'
 import CharacterLifeArea from '../CharacterLifeArea'
 
 import { CharacterDaraAreaStyled, CharacterEffectFlex } from './style.module'
 
-const CharacterDataArea = () => {
+const CharacterDataArea = ({ id, life, effectsIds }: ICharacter) => {
+  const { name } = getItem(charactersList, id)
+
   return (
     <CharacterDaraAreaStyled>
       <CharacterEffectFlex>
-        <span>Naruto</span>
-        <CharacterEffectsArea />
+        <span>{name}</span>
+        <CharacterEffectsArea effectsIds={effectsIds} />
       </CharacterEffectFlex>
-      <CharacterLifeArea />
+      <CharacterLifeArea life={life} />
     </CharacterDaraAreaStyled>
   )
 }
