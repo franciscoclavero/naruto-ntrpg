8e7b55f90a72d32cd1a52400856c0588f2cddfda
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-22 11:51:54 -0300

    feat: create character effect styled

--- src/components/molecules/CharacterEffect/index.tsx
+++ src/components/molecules/CharacterEffect/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
+import { CharacterEffectStyle } from './style.module'
+
 const CharacterEffect = () => {
-  return <div>Effects</div>
+  return (
+    <CharacterEffectStyle>
+      <abbr title='Nome Effect'>
+        <img src='assets/characters/naruto.jpg' alt='Effect' />
+      </abbr>
+    </CharacterEffectStyle>
+  )
 }
 
 export default CharacterEffect
