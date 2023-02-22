29f3e4a6cc9582d1dc9631dae6f38b89cb976627
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-22 11:53:48 -0300

    feat: create effect area style

--- src/components/molecules/CharacterEffectsArea/index.tsx
+++ src/components/molecules/CharacterEffectsArea/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
 
+import CharacterEffect from '../CharacterEffect'
+
+import { CharacterEffectAreaStyle } from './style.module'
+
 const CharacterEffectsArea = () => {
-  return <div>Effects</div>
+  return (
+    <CharacterEffectAreaStyle>
+      <CharacterEffect />
+      <CharacterEffect />
+    </CharacterEffectAreaStyle>
+  )
 }
 
 export default CharacterEffectsArea
