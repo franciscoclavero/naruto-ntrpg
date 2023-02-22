24b9feb63ef3b29cff285a780e734b2ea074dedc
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-22 11:54:35 -0300

    feat: create Character Area styled

--- src/components/molecules/CharacterArea/index.tsx
+++ src/components/molecules/CharacterArea/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import CharacterDataArea from '../CharacterDataArea'
+import CharacterImage from '../CharacterImage'
+
+import { CharacterAreaStyled } from './style.module'
 
 const CharacterArea = () => {
-  return <div></div>
+  return (
+    <CharacterAreaStyled>
+      <CharacterImage />
+      <CharacterDataArea />
+    </CharacterAreaStyled>
+  )
 }
 
 export default CharacterArea
