8085f91eab2ed7c8d5136d555229853e29436d75
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-22 14:42:02 -0300

    feat: change name function

--- src/functions/getItem.tsx
+++ src/functions/getItem.tsx
@@ -1,5 +1,5 @@
 import { IItem } from '../DB/charactersList'
 
-export function getCharacter(array: IItem[], id: number) {
+export function getItem(array: IItem[], id: number) {
   return array.filter((item) => item.id === id)[0]
 }
