1fd4f91991bc20d5bba788bb970b6428d3a0b809
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-22 10:48:15 -0300

    feat: create style character image

---
+++ src/components/molecules/CharacterImage/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { CharacterImageStyled } from './style.module'
+
+const CharacterImage = () => {
+  return <CharacterImageStyled src='assets/characters/naruto.jpg' />
+}
+
+export default CharacterImage
