1c8ad5612898e13aa0d2f0dabfa00803e507e431
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-22 13:34:21 -0300

    feat: create character and effect values

---
+++ src/DB/characters.tsx
@@ -0,0 +1,23 @@
+export interface IItem {
+  id: number
+  name: string
+  sprite: string
+}
+
+export const characters: IItem[] = [
+  {
+    id: 1,
+    name: 'Naruto',
+    sprite: 'assets/characters/naruto.jpg',
+  },
+  {
+    id: 2,
+    name: 'Sasuke',
+    sprite: 'assets/characters/naruto.jpg',
+  },
+  {
+    id: 3,
+    name: 'Sakura',
+    sprite: 'assets/characters/naruto.jpg',
+  },
+]
