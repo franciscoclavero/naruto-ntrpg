569158ff5de49aa8f5ca8a61a8420ab8b895f999
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-22 14:43:16 -0300

    feat: create effects component

--- src/components/molecules/CharacterEffectsArea/index.tsx
+++ src/components/molecules/CharacterEffectsArea/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react'
+import { effectsList } from '../../../DB/effectsList'
+import { getItem } from '../../../functions/getItem'
 
 import CharacterEffect from '../CharacterEffect'
 
 import { CharacterEffectAreaStyle } from './style.module'
 
-const CharacterEffectsArea = () => {
+interface IEffects {
+  effectsIds: number[]
+}
+
+const CharacterEffectsArea = ({ effectsIds }: IEffects) => {
   return (
     <CharacterEffectAreaStyle>
-      <CharacterEffect />
-      <CharacterEffect />
+      {effectsIds.map((id) => {
+        const effect = getItem(effectsList, id)
+        return <CharacterEffect key={id} name={effect.name} sprite={effect.sprite} />
+      })}
     </CharacterEffectAreaStyle>
   )
 }
