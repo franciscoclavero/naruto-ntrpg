d37888ed512bf3ca36de0862fb6180ee47bb0a7d
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-23 17:06:16 -0300

    feat: change tests to use redux

--- src/components/molecules/Map/index.test.tsx
+++ src/components/molecules/Map/index.test.tsx
@@ -3,13 +3,17 @@ import '@testing-library/react'
 import React from 'react'
 
 import Map from './'
+import { store } from '../../../redux/store'
+import { Provider } from 'react-redux'
 
 describe('Map component', () => {
   it('- render without background', () => {
     const typeItens = [{ posX: 0, posY: 0, sprite: '' }]
 
     const { container } = render(
-      <Map numberLines={0} numberColumns={0} widthSize='400px' typeItens={typeItens} />,
+      <Provider store={store}>
+        <Map numberLines={0} numberColumns={0} widthSize='400px' typeItens={typeItens} />
+      </Provider>,
     )
 
     const mapNodes = container.getElementsByTagName('div')
@@ -20,7 +24,9 @@ describe('Map component', () => {
     const typeItens = [{ posX: 0, posY: 0, sprite: 'naruto' }]
 
     const { container } = render(
-      <Map numberLines={2} numberColumns={2} widthSize='400px' typeItens={typeItens} />,
+      <Provider store={store}>
+        <Map numberLines={2} numberColumns={2} widthSize='400px' typeItens={typeItens} />
+      </Provider>,
     )
 
     const mapNodes = container.getElementsByTagName('div')
--- src/components/molecules/MapField/index.test.tsx
+++ src/components/molecules/MapField/index.test.tsx
@@ -1,13 +1,17 @@
-import { screen, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
 import React from 'react'
 
 import MapField from './'
+import { Provider } from 'react-redux'
+import { store } from '../../../redux/store'
 
 describe('MapField component ', () => {
   it('- render', () => {
     const { container } = render(
-      <MapField numberColumns={0} numberLines={0} sprite='' widthSize='80px' />,
+      <Provider store={store}>
+        <MapField numberColumns={0} numberLines={0} sprite='' widthSize='80px' />
+      </Provider>,
     )
 
     const mapFieldNodes = container.getElementsByTagName('div')
