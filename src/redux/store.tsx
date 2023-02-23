5829b3c6e76f9d081e0fd03b863fe98d829ce61d
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-23 16:26:30 -0300

    feat: create chamarter menu reducer em add in store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 
 import timerReducer from './reducers/timerReducer'
+import characterMenuOptionsReducer from './reducers/characterMenuOptionsReducer'
 
 export const store = configureStore({
   reducer: {
     timer: timerReducer,
+    characterMenu: characterMenuOptionsReducer,
   },
 })
 
