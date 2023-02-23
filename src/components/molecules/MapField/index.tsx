d1c5094b4cbb5191024eeba7d5354badb5551191
Francisco Clavero <franciscogkoclavero@gmail.com>
2023-02-23 17:05:30 -0300

    feat: set store

--- src/components/molecules/MapField/index.tsx
+++ src/components/molecules/MapField/index.tsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useDispatch } from 'react-redux'
+
+import { setLeft, setTop, setVisible } from '../../../redux/reducers/characterMenuOptionsReducer'
 
 import { MapFieldStyled } from './style.module'
 
@@ -10,12 +13,27 @@ export interface IMapField {
 }
 
 const MapField = ({ numberLines, numberColumns, widthSize, sprite }: IMapField) => {
+  const offsetData = useRef<HTMLDivElement>(null)
+  const dispatch = useDispatch()
+
+  const handleClick = () => {
+    if (offsetData.current != null) {
+      const fieldSize = offsetData.current.clientWidth
+
+      dispatch(setVisible(true))
+      dispatch(setLeft(offsetData.current.offsetLeft + fieldSize + 10))
+      dispatch(setTop(offsetData.current.offsetTop))
+    }
+  }
+
   return (
     <MapFieldStyled
+      ref={offsetData}
       numberColumns={numberColumns}
       numberLines={numberLines}
       widthSize={widthSize}
       sprite={sprite}
+      onClick={handleClick}
     >
       {sprite ? <img src={`${sprite != '' ? 'assets/' + sprite : ''}`} /> : ''}
     </MapFieldStyled>
